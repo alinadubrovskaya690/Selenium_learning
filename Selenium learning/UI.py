5e69937a5d44d3578f7d9ee45e3273f7c75e5fbc
alinadubrovskaya690 <elenashamkina00@gmail.com>
2025-09-19 10:51:41 +0300

    Project changes

--- Selenium learning/UI.py
+++ Selenium learning/UI.py
@@ -3,7 +3,6 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import Select
 import time
 
 service = Service(ChromeDriverManager().install())
