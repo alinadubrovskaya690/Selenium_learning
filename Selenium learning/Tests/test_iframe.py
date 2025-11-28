b9fd30a780107c8b817fd8695fadb642937fb472
alinadubrovskaya690 <elenashamkina00@gmail.com>
2025-11-28 09:49:34 +0300

    Project changes 2

--- Selenium learning/Tests/test_iframe.py
+++ Selenium learning/Tests/test_iframe.py
@@ -5,3 +5,29 @@ def test_navbar_btn(browser):
     iframe_pg.iframe_open()
     iframe_pg.iframe_switch()
     iframe_pg.navbar_button()
+
+def test_home_btn(browser):
+    iframe_pg = IframePage(browser)
+    iframe_pg.iframe_open()
+    iframe_pg.iframe_switch()
+    iframe_pg.home_button()
+
+def test_features_btn(browser):
+    iframe_pg = IframePage(browser)
+    iframe_pg.iframe_open()
+    iframe_pg.iframe_switch()
+    iframe_pg.features_button()
+
+def test_pricing_btn(browser):
+    iframe_pg = IframePage(browser)
+    iframe_pg.iframe_open()
+    iframe_pg.iframe_switch()
+    iframe_pg.pricing_button()
+
+def test_learn_more_btn(browser):
+    iframe_pg = IframePage(browser)
+    iframe_pg.iframe_open()
+    iframe_pg.iframe_switch()
+    iframe_pg.learn_more_button()
+    assert iframe_pg.learn_more_text_is_displayed() == True
+    assert iframe_pg.learn_more_text() == 'This text appears when you click the "Learn more" button'
